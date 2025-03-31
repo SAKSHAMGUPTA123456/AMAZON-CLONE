60fa7aa7ef9b38ebff868d2b39de07c13419b017
SAKSHAMGUPTA123456 <sakshamgupta0155.in>
2025-03-31 15:45:29 +0530

    added

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,55 @@
 import { NavLink } from "react-router-dom"
-
+import k from './assest/download.png'
+import icon from './assest/download (2).png'
+import { useState } from "react"
+import { motion } from "framer-motion"
 export const Header=()=>{
+    const [iconclick,newiconclick]=useState(false)
+    const handleclick=()=>{
+        newiconclick(!iconclick)
+    }
     return(
       <>
-      <div>
-
-       <NavLink to="sell"> <h1>SELL</h1></NavLink>
+  <motion.div
+      initial={{ x: -350 }}
+      animate={{ x: iconclick ? 0 : -350 }}
+      transition={{ type: "spring", stiffness: 100 }}
+      className="fixed top-0 left-0 h-full w-[350px] shadow-lg"
+    >
+    
+      <div style={{ backgroundColor: "#232f3e", border: "2px solid white", padding: "10px" }}>
+        <div style={{ color: "white" }}>Browse</div>
+        <div style={{ fontSize: "20px", color: "white" }}>Amazon</div>
+</div>
+<div class="flex justify-between">
+<div class="bg-black">
+      <div style={{ color: "white", fontSize: "30px", padding: "10px" }}>Amazon Home</div>
+      {[
+        "Trending",
+        "Bestsellers",
+        "New Releases",
+        "Movers and Shakers",
+        "Mobiles",
+        "Computers",
+        "Books",
+        "Amazon Fashion",
+      ].map((item) => (
+        <div key={item} style={{ color: "white", height: "40px", padding: "10px" }}>{item}</div>
+      ))}
+   </div>
+    <div onClick={()=>handleclick()}>
+        <h1 style={{border:"2px solid blue"}}>X</h1>
       </div>
+      </div>
+      </motion.div>
+      <div style={{backgroundColor:"#232f3e"}}>
+      <div class="flex justify-between">
+       <div onClick={()=>handleclick()}><img src={icon} style={{width:"30px"}} class="mt-3" ></img></div>
+      <div class="mt-3"><img  src={k} style={{width:"120px"}} class=""></img></div>
+    
+      </div>
+      </div>
+
       </>
     )
 }
