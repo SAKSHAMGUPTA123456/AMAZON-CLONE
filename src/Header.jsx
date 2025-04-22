52ef24b66884525b36f39462c7c1460c25828907
SAKSHAMGUPTA123456 <sakshamgupta0155.in>
2025-04-22 13:44:28 +0530

    s

--- src/Header.jsx
+++ src/Header.jsx
@@ -14,7 +14,7 @@ export const Header = ({ setSidebarOpen, sidebarOpen }) => {
     setSidebarOpen(!sidebarOpen);
   };
   const gf=useSelector((state)=>state.task.task)
-  const gh=gf.length||localStorage.length
+  const gh=gf.length||(localStorage.length-1)
   return (
     <>
       {/* Sidebar */}
