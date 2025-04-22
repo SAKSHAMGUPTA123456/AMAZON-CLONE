500ae03e5d2fb52bd2ca886e3b7595ebb715b16a
SAKSHAMGUPTA123456 <sakshamgupta0155.in>
2025-04-22 17:48:56 +0530

    s

--- src/Aisupport.jsx
+++ src/Aisupport.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react"
+
 export const Ai = () => {
     const fg=window.innerWidth-50
-    console.log(fg)
+  
+    const color=["orange","red","yellow","pink"]
+    const [newfirst,old]=useState("")
+    setTimeout(() => {
+        let rt=parseInt(Math.random()*4)
+console.log(rt)
+        old(color[rt])
+
+    }, 2000);
     return (
 
-       <div class=" z-[1100]" style={{position:"fixed",left:`${fg}px`}}> 
-        <button style={{width:"40px",backgroundColor:"orange"}}>
+       <div class=" z-[1100] " style={{position:"fixed",top:"95px",left:`${fg}px`}}> 
+        <button style={{width:"40px",backgroundColor:`${newfirst}`}}>
           WANT ANSWERS FROM AI
         </button>
         </div>
